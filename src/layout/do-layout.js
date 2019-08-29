2dace15ef457979850c40e92f11b2fab0bf53b12
zhanning.bzn <zhanning.bzn@antfin.com>
2019-08-29 10:04:22 +0800

    fix eslint

--- src/layout/do-layout.js
+++ src/layout/do-layout.js
@@ -62,7 +62,7 @@ module.exports = (root, options, layoutAlgrithm) => {
     }
   }
   // fixed root position, default value is false
-  const { fixedRoot } = options
+  const { fixedRoot } = options;
   if (!fixedRoot) {
     root.translate(-(root.x + root.width / 2 + root.hgap), -(root.y + root.height / 2 + root.vgap));
   }
